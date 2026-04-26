f1f56bab69da731d83c1573b3c802d34b260b44c
Anthony Cruz <tony_esqueloto@hotmail.com>
2026-04-26 18:40:54 -0300

    feat: adiciona mapstruct e configura uma inmterface para atualizacao parcial da entidade item

--- src/main/java/com/anthony/blacksmithOnlineStore/mapstruct/ItemUpdate.java
+++ src/main/java/com/anthony/blacksmithOnlineStore/mapstruct/ItemUpdate.java
@@ -1,5 +1,15 @@
-package com.anthony.blacksmithOnlineStore.mapStruct;
+package com.anthony.blacksmithOnlineStore.mapstruct;
 
-public class ItemUpdate {
+import com.anthony.blacksmithOnlineStore.controler.dto.item.ItemPatchUpdateDto;
+import com.anthony.blacksmithOnlineStore.entity.Item;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
+@Mapper(componentModel = "spring")
+public interface ItemUpdate {
+
+  @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+  void updateItemFromDto(ItemPatchUpdateDto dto, @MappingTarget Item item);
 }
