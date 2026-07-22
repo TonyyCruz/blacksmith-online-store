b322d4bf50806159916136c054710cee62b063db
Anthony Cruz <tony_esqueloto@hotmail.com>
2026-07-22 19:34:42 -0300

    chore: remove o metodo "isRated" da entidade OrdeItem e usa uma verificação de existsByOrderItemId no RatingRepository

--- src/main/java/com/anthony/blacksmithOnlineStore/repository/RatingRepository.java
+++ src/main/java/com/anthony/blacksmithOnlineStore/repository/RatingRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RatingRepository extends JpaRepository<Rating, Long> {
 
   Page<Rating> findAllByReviewedItemId(long reviewedItemId, Pageable pageable);
+
+  boolean existsByOrderItemId(Long id);
 }
