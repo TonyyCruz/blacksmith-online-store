fdd1a223dd5fd6586b90a42f2558a3ea41d50856
Anthony Cruz <tony_esqueloto@hotmail.com>
2026-08-02 19:49:49 -0300

    chore: atualiza validacoes do item patch update

--- src/main/java/com/anthony/blacksmithOnlineStore/controller/dto/item/ItemPatchUpdateDto.java
+++ src/main/java/com/anthony/blacksmithOnlineStore/controller/dto/item/ItemPatchUpdateDto.java
@@ -5,7 +5,8 @@ import com.anthony.blacksmithOnlineStore.enums.Rarity;
 import com.anthony.blacksmithOnlineStore.enums.Type;
 import com.anthony.blacksmithOnlineStore.validations.item.NullOrRange;
 import com.anthony.blacksmithOnlineStore.validations.item.NullOrSize;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Builder;
 
@@ -14,16 +15,18 @@ public record ItemPatchUpdateDto(
     @NullOrSize(min = 2, message = "Name must have at lest 2 characters") String name,
     Material material,
     @NullOrRange(min = 1, message = "Base damage must be at lest 1") Integer baseDamage,
-    @Positive(message = "Base defense must not be negative") Integer baseDefense,
-    @Positive(message = "Base price must not be a negative number") BigDecimal basePrice,
-    @Positive(message = "Final price must not be a negative number") BigDecimal finalPrice,
-    @NullOrSize(min = 10, message = "Description must have at lest 10 characters") String description,
-    @Positive(message = "Weight price must not be a negative number") Float weight,
-    @Positive(message = "Stock price must not be a negative number") Integer stock,
-    Type type,
-    Rarity rarity,
-    Long blacksmithId,
-    Boolean active
+    @Min(value = 1, message = "Base defense must not be negative") Integer baseDefense,
+    @Min(value = 1, message = "Base price must not be a negative number") BigDecimal basePrice,
+    @Min(value = 1, message = "Final price must not be a negative number") BigDecimal finalPrice,
+    @NullOrSize(min = 10, message = "Description must have at lest 10 characters")
+    String description,
+    @Min(value = 1, message = "Weight price must not be a negative number") Float weight,
+    @Min(value = 1, message = "Stock price must not be a negative number") Integer stock,
+    @NotNull Type type,
+    @NotNull Rarity rarity,
+    @Min(value = 1, message = "Blacksmith id must be a positive number")
+    @NotNull Long blacksmithId,
+    @NotNull Boolean active
 ) {
 
 }
