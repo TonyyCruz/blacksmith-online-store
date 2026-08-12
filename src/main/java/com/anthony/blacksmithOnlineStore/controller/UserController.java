c7642994318721855100451170abf6ebdc7969a5
tony <tony_esqueloto@hotmail.com>
2026-08-12 14:17:24 -0300

    docs: adiciona interface para documentacao de user controller

--- src/main/java/com/anthony/blacksmithOnlineStore/controller/UserController.java
+++ src/main/java/com/anthony/blacksmithOnlineStore/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.anthony.blacksmithOnlineStore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,47 +9,43 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anthony.blacksmithOnlineStore.controller.docs.UserControllerDocs;
 import com.anthony.blacksmithOnlineStore.controller.dto.user.PasswordUpdateDto;
 import com.anthony.blacksmithOnlineStore.controller.dto.user.UserDto;
 import com.anthony.blacksmithOnlineStore.controller.dto.user.UserUpdateDto;
 import com.anthony.blacksmithOnlineStore.service.UserService;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/users")
-@SecurityRequirement(name = "bearerAuth")
-@Tag(name = "Users", description = "User management")
-public class UserController {
+public class UserController implements UserControllerDocs {
   private final UserService userService;
 
+  @Autowired
   @GetMapping("/me")
-  @Operation(summary = "Get self user data")
   public ResponseEntity<UserDto> getCurrentUser() {
     return ResponseEntity.ok(userService.getUser());
   }
 
+  @Autowired
   @PutMapping("/me")
-  @Operation(summary = "Update all self user data")
   public ResponseEntity<UserDto> updateCurrentUser(@RequestBody @Valid UserUpdateDto updateDto) {
     return ResponseEntity.ok(userService.updateUser(updateDto));
   }
 
+  @Autowired
   @PutMapping("/me/password")
-  @Operation(summary = "Update self users password")
   public ResponseEntity<Void> updateCurrentUserPassword(
       @RequestBody @Valid PasswordUpdateDto passwordUpdateDto) {
     userService.updatePassword(passwordUpdateDto);
     return ResponseEntity.noContent().build();
   }
 
+  @Autowired
   @DeleteMapping("/me")
-  @Operation(summary = "Delete self user")
   public ResponseEntity<Void> deleteCurrentUser() {
     userService.deleteUserFromAuth();
     return ResponseEntity.noContent().build();
