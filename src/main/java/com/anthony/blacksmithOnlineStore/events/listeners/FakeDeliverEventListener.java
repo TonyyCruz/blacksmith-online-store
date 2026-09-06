ce75d915c58882559059830b8834ff592cd9ae13
Anthony Cruz <tony_esqueloto@hotmail.com>
2026-09-06 19:46:25 -0300

    chore: adiciona comportamento ascincrono na simulacao de entrega

--- src/main/java/com/anthony/blacksmithOnlineStore/events/listeners/FakeDeliverEventListener.java
+++ src/main/java/com/anthony/blacksmithOnlineStore/events/listeners/FakeDeliverEventListener.java
@@ -1,29 +1,26 @@
 package com.anthony.blacksmithOnlineStore.events.listeners;
 
+import com.anthony.blacksmithOnlineStore.entity.Order;
+import com.anthony.blacksmithOnlineStore.enums.OrderStatus;
+import com.anthony.blacksmithOnlineStore.events.OrderPaidEvent;
+import com.anthony.blacksmithOnlineStore.events.ReturnRequestEvent;
+import com.anthony.blacksmithOnlineStore.exceptions.BusinessViolationException;
+import com.anthony.blacksmithOnlineStore.service.OrderService;
 import java.time.LocalDateTime;
-
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-import com.anthony.blacksmithOnlineStore.entity.Order;
-import com.anthony.blacksmithOnlineStore.enums.OrderStatus;
-import com.anthony.blacksmithOnlineStore.events.OrderPaidEvent;
-import com.anthony.blacksmithOnlineStore.events.ReturnRequestEvent;
-import com.anthony.blacksmithOnlineStore.exceptions.BusinessViolationException;
-import com.anthony.blacksmithOnlineStore.repository.OrderRepository;
-import com.anthony.blacksmithOnlineStore.service.OrderService;
-
-import lombok.RequiredArgsConstructor;
-
 @Component
 @RequiredArgsConstructor
 public class FakeDeliverEventListener {
-  private final OrderRepository orderRepository;
   private final OrderService orderService;
 
+  @Async
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   @Transactional(propagation = Propagation.REQUIRES_NEW)
   public void deliverRequest(OrderPaidEvent paidEvent) {
@@ -34,24 +31,33 @@ public class FakeDeliverEventListener {
     if (order.getDeliveredAt() != null) {
       throw new BusinessViolationException("This order has already been delivered");
     }
-    order.setDeliveredAt(LocalDateTime.now());
     order.setStatus(OrderStatus.SEPARATING);
     order.setStatus(OrderStatus.DISPATCHED);
     order.setStatus(OrderStatus.IN_TRANSIT);
     order.setStatus(OrderStatus.OUT_FOR_DELIVERY);
     order.setStatus(OrderStatus.DELIVERED);
-    orderRepository.save(order);
+    simulateProcessingTime();
+    order.setDeliveredAt(LocalDateTime.now());
   }
 
+  @Async
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   @Transactional(propagation = Propagation.REQUIRES_NEW)
   public void returnRequest(ReturnRequestEvent returnEvent) {
-    Order order = orderService.findSelfEntityById(returnEvent.orderId());
+    Order order = orderService.findEntityById(returnEvent.orderId());
     if (!OrderStatus.DELIVERED.equals(order.getStatus())) {
       throw new BusinessViolationException("A not delivered order cannot be returned");
     }
     order.setStatus(OrderStatus.RETURN_REQUESTED);
     order.setStatus(OrderStatus.RETURNED);
-    orderRepository.save(order);
+    simulateProcessingTime();
+  }
+
+  private void simulateProcessingTime() {
+    try {
+      Thread.sleep(10000); // Simulate some processing time
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
   }
 }
